0aac22ab19b9f857cfa28c23cc8866beacfb9241
arrtcy <2510356829@qq.com>
2020-11-21 12:42:37 +0800

    基础搭建

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import NavBottom from "./components/NavBottom";
+import SearchTop from "./components/SearchTop";
+import Main from "./Main";
+import { Switch, Route, withRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <SearchTop />
+      <Main></Main>
+      <NavBottom />
+    </>
   );
 }
 
-export default App;
+export default withRouter(App);
